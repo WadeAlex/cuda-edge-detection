d4bde3857c9921d5fa42913fff0ff66f18da0969
WadeAlex <WadeAlex@b5bda9f4-262e-11de-8205-99c4e13cf704>
2009-04-14 03:05:58 +0000

    Got image loading and beginning of magnitude computation stuff done.

--- trunk/EdgeDetection.cpp
+++ trunk/EdgeDetection.cpp
@@ -1,5 +1,6 @@
 #include "EdgeDetection.h"
 #include "Convolution.h"
+#include "ImageHandler.h"
 
 #include <iostream>
 
@@ -17,18 +18,42 @@ EdgeDetection::~EdgeDetection()
 
 void EdgeDetection::loadInputImage(const char* filename)
 {
-
+	this->imgHandler.loadImage(filename);
 }
 
 void EdgeDetection::performEdgeDetection()
 {
-	performConvolution(0, 0, 0);
+	unsigned* gradientMagnitude = new unsigned[this->imgHandler.getImagePixelCount()];
+	computeImageGradient(this->imgHandler.getMatrix(), gradientMagnitude);
+	delete[] gradientMagnitude;
 }
 
 void EdgeDetection::exportEdgeImage(const char* filename) const
 {
 
 }
+
+void EdgeDetection::computeImageGradient(uint16_t* imageMatrix, unsigned* imageGradient)
+{ 
+	// To be done on both CPU and GPU.
+	// int* xGradient = convolution(imgHandler->getMatrix(), xGradientMask);
+	// int* yGradient = convolution(imgHandler->getMatrix(), yGradientMask);
+	
+}
+
+const int EdgeDetection::xGradientMask[9] = 
+{
+	-1, 0, 1,
+	-2, 0, 2,
+	-1, 0, 1
+};
+
+const int EdgeDetection::yGradientMask[9] = 
+{
+	1, 2, 1,
+	0, 0, 0,
+	-1, -2, -1
+};
 
 int main(char** argv, int argc)
 {
@@ -40,9 +65,12 @@ int main(char** argv, int argc)
 
 	EdgeDetection edgeDetector;
 
-	edgeDetector.loadInputImage(/*argv[0]*/"");
+	edgeDetector.loadInputImage(/*argv[0]*/"c:\\development\\CDA6938\\project2\\test.png");
 	edgeDetector.performEdgeDetection();
 	edgeDetector.exportEdgeImage(/*argv[1]*/"");
+
+	cout << "Press any key to exit..." << endl;
+	getchar();
 
 	return 0;
 }
