18d9ea89e33b7c8240ec0632b3bb9d0f331aef46
WadeAlex <WadeAlex@b5bda9f4-262e-11de-8205-99c4e13cf704>
2009-04-20 06:37:08 +0000

    Got CPU implementation working.

--- trunk/ImageHandler.cpp
+++ trunk/ImageHandler.cpp
@@ -28,14 +28,14 @@ void ImageHandler::loadImage(const string& filename)
 		Magick::Image img;
 		img.read(filename);
 		this->imageWidth = img.rows();
-		this->matrix = new uint16_t[getImagePixelCount()];
+		this->matrix = new float[getImagePixelCount()];
 		Magick::PixelPacket* imgPixels = img.getPixels(0, 0, img.columns(), img.rows());
 		for(unsigned i = 0; i < img.rows(); ++i)
 		{
 			for(unsigned j = 0; j < img.columns(); ++j)
 			{
 				unsigned index = i * img.columns() + j;
-				this->matrix[index] = imgPixels[index].red;
+				this->matrix[index] = static_cast<float>(imgPixels[index].red);
 			}
 		}
 	}
@@ -45,20 +45,30 @@ void ImageHandler::loadImage(const string& filename)
 	}
 }
 
-uint16_t* ImageHandler::getMatrix() const
+float* ImageHandler::getMatrix()
 {
 	return this->matrix;
 }
 
-void ImageHandler::writeImage(uint16_t* imageMatrix, unsigned imageWidth, const string& filename) const
+const float* ImageHandler::getMatrix() const
 {
+	return this->matrix;
+}
+
+void ImageHandler::writeImage(float* imageMatrix, const string& filename, bool rescaleImage) const
+{
+	if(rescaleImage)
+	{
+		rescale(imageMatrix);
+	}
+
 	stringstream imageResolution;
 	imageResolution << imageWidth << 'x' << imageWidth;
 	
 	try
 	{
-		Magick::Image outputImage(imageResolution.str(), "black");
-		outputImage.type(Magick::GrayscaleType);
+		Magick::Image outputImage(imageResolution.str(), "white");
+		//outputImage.type(Magick::GrayscaleType);
 
 		Magick::PixelPacket* outputImagePixels = outputImage.getPixels(0, 0, outputImage.columns(), outputImage.rows());
 		for(unsigned i = 0; i < imageWidth; ++i)
@@ -68,8 +78,53 @@ void ImageHandler::writeImage(uint16_t* imageMatrix, unsigned imageWidth, const 
 				unsigned pixelIndex = (i * imageWidth + j);
 				outputImagePixels[pixelIndex].red = 
 					outputImagePixels[pixelIndex].green = 
-					outputImagePixels[pixelIndex].blue = 
-					imageMatrix[pixelIndex];
+					outputImagePixels[pixelIndex].blue =
+					imageMatrix[pixelIndex] * 65535;
+			}
+		}
+
+		outputImage.write(filename);
+	}
+	catch(Magick::Exception& e)
+	{
+		cerr << "Caught exception: " << e.what() << endl;
+	}
+}
+
+void ImageHandler::writeEdgeDirectionImage(unsigned* edgeDirections, float* intensities, const string& filename, float intensityThreshold) const
+{
+	stringstream imageResolution;
+	imageResolution << imageWidth << 'x' << imageWidth;
+
+	try
+	{
+		Magick::Image outputImage(imageResolution.str(), "black");
+		//outputImage.type(Magick::GrayscaleType);
+
+		Magick::PixelPacket* outputImagePixels = outputImage.getPixels(0, 0, outputImage.columns(), outputImage.rows());
+		for(unsigned i = 0; i < imageWidth; ++i)
+		{
+			for(unsigned j = 0; j < imageWidth; ++j)
+			{
+				unsigned pixelIndex = (i * imageWidth + j);
+				if(intensities[pixelIndex] >= intensityThreshold)
+				{
+					switch(edgeDirections[pixelIndex])
+					{
+						case 0:
+							outputImagePixels[pixelIndex].blue = 65535;
+							break;
+						case 1:
+							outputImagePixels[pixelIndex].red = 65535;
+							 break;
+						case 2:
+							outputImagePixels[pixelIndex].green = 65535;
+							break;
+						case 3:
+							outputImagePixels[pixelIndex].red =	outputImagePixels[pixelIndex].green = 65535;
+							break;
+					}
+				}
 			}
 		}
 
@@ -89,4 +144,32 @@ unsigned ImageHandler::getImagePixelCount() const
 unsigned ImageHandler::getImageWidth() const
 {
 	return this->imageWidth;
+}
+
+void ImageHandler::setMatrix(float* newMatrix)
+{
+	delete[] this->matrix;
+	this->matrix = newMatrix;
+}
+
+float ImageHandler::getMaximumValue(float* matrix) const
+{
+	float maximumValue = 0;
+	for(unsigned i = 0; i < getImagePixelCount(); ++i)
+	{
+		if(matrix[i] > maximumValue)
+		{
+			maximumValue = matrix[i];
+		}
+	}
+	return maximumValue;
+}
+
+void ImageHandler::rescale(float* matrix) const
+{
+	float maximumValue = getMaximumValue(matrix);
+	for(unsigned i = 0; i < getImagePixelCount(); ++i)
+	{
+		matrix[i] /= maximumValue;
+	}
 }
